5e6cd727e092305936843d69794e0ea75ef7b818
Banchan Sangma <bsangma32@gmail.com>
2020-07-19 03:04:22 +0530

    title update

--- src/server.js
+++ src/server.js
@@ -101,7 +101,7 @@ function renderFullPage(markup, preloadedState) {
       <head>
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta charset="utf-8" />
-          <title>Welcome to Razzle</title>
+          <title>Daily News</title>
           <meta name="viewport" content="width=device-width, initial-scale=1">
           ${
             assets.client.css
